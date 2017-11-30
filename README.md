c85191c47e85190c9432911360a02fa258f28840
cody <cevek@ya.ru>
2017-11-30 15:20:28 +0500

    Prepare to publish

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # ttypescript
+## What it is
+Currently TypeScript doesn't support custom transformers in the tsconfig.json, but supports its programatically.
+
+And there is no way to compile your files using custom transformers using `tsc` command.
+
+TTypescript (Transformer Typescript) solves this problem by patching on the fly the compile module to use transformers from `tsconfig.json`.
+
+## How to install
+```
+npm i ttypescript --save
+npm i -g ttypescript
+```
+ttypescript uses your installed `typescript` in your `node_modules`
+
+## How to use
+
+### tsconfig.json
+You just need to add the `customTransformers` block with `before` and(or) `after` object contains array of transformer paths into the `compilerOptions`
+```json
+{
+  "compilerOptions": {
+   "customTransformers": {
+      "before": [
+        "transformermodule"
+      ],
+      "after": [
+        "./or_some_path_in_your_project_directory"
+      ]
+    }
+  }
+}
+```
+
+### Command line
+Like usual `tsc`, all arguments work the same way.
+```
+ttsc
+```
+
+### ts-node
+```
+ts-node --compiler ttypescript index.ts
+or
+ts-node -C ttypescript index.ts
+```
+### Webpack
+```js
+    {
+        test: /\.(ts|tsx)$/,
+        include: paths.appSrc,
+        loader: require.resolve('ts-loader'),
+        options: {
+            compiler: 'ttypescript'
+        }
+    }
+```
+
+### Visual Studio Code
+Set the config `typescript.tsdk` to `/usr/local/lib/node_modules/ttypescript/lib/`
+
+### WebStorm
+Set in the TypeScript settings tab the typescript path: `/usr/local/lib/node_modules/ttypescript` or from your project node_modules
+
+## License
+MIT License
--- postinstall.js
+++ postinstall.js
@@ -1,45 +1,14 @@
 var fs = require('fs');
 var path = require('path');
 
-const tsFolder = path.dirname(require.resolve('typescript/lib/tsc'));
-try {
-    fs.mkdirSync(__dirname + '/lib');
-} catch (e) {}
-copyFolderRecursiveSync(tsFolder, './lib/');
-
-function copyFileSync( source, target ) {
-
-    var targetFile = target;
-
-    //if target is a directory a new file with the same name will be created
-    if ( fs.existsSync( target ) ) {
-        if ( fs.lstatSync( target ).isDirectory() ) {
-            targetFile = path.join( target, path.basename( source ) );
-        }
+const tsLibDir = path.dirname(require.resolve('typescript/lib/tsc')) + '/';
+const libContent = fs.readFileSync(tsLibDir + 'tsserverlibrary.js', 'utf8');
+const appendContent = fs.readFileSync(__dirname + '/lib/patch.js', 'utf8') + '(ts)';
+fs.writeFileSync(__dirname + '/lib/tsserverlibrary.js', libContent + '\n' + appendContent);
+fs.readdirSync(tsLibDir).forEach(file => {
+    if (file.match(/\.d\.ts$/)) {
+        const content = fs.readFileSync(tsLibDir + file, 'utf8');
+        fs.writeFileSync(__dirname + '/lib/' + file, content);
     }
-
-    fs.writeFileSync(targetFile, fs.readFileSync(source));
-}
-
-function copyFolderRecursiveSync( source, target ) {
-    var files = [];
-
-    //check if folder needs to be created or integrated
-    var targetFolder = path.join( target, path.basename( source ) );
-    if ( !fs.existsSync( targetFolder ) ) {
-        fs.mkdirSync( targetFolder );
-    }
-
-    //copy
-    if ( fs.lstatSync( source ).isDirectory() ) {
-        files = fs.readdirSync( source );
-        files.forEach( function ( file ) {
-            var curSource = path.join( source, file );
-            if ( fs.lstatSync( curSource ).isDirectory() ) {
-                copyFolderRecursiveSync( curSource, targetFolder );
-            } else {
-                copyFileSync( curSource, targetFolder );
-            }
-        } );
-    }
-}
+});
+console.log('ttypescript: installed TS version: ' + require('typescript').version);
