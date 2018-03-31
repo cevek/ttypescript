6ca5930bcc725ff9a12e274f03d49fe44fe670a7
cody <cevek@ya.ru>
2018-03-31 16:14:55 +0500

    Refactor to use typescript from cwd folder

---
+++ lib/customRequire.ts
@@ -0,0 +1,13 @@
+import Module from 'module';
+export function customRequireResolve(dir: string, module: string) {
+    const m = new Module('');
+    m.filename = dir + '/index.js';
+    m.paths = (Module as any)._nodeModulePaths(dir);
+    return (Module as any)._resolveFilename(module, m);
+}
+export function customRequire(dir: string, module: string) {
+    const m = new Module('');
+    m.filename = dir + '/index.js';
+    m.paths = (Module as any)._nodeModulePaths(dir);
+    return (Module as any)._load(module, m);
+}
