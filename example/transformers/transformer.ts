a4216d00a5138b252ab010efe128a916fd80e8b8
cody <cevek@ya.ru>
2018-05-06 14:42:42 +0500

    Fix example

--- example/transformers/transformer.ts
+++ example/transformers/transformer.ts
@@ -2,7 +2,7 @@ import * as ts from 'typescript';
 export default (ctx: ts.TransformationContext, program: ts.Program): ts.Transformer<ts.SourceFile> => {
     return sourceFile => {
         function visitor(node: ts.Node): ts.Node {
-            if (ts.isCallExpression(node) && ts.getTextOfNodeFromSourceText(sourceFile.text, node.expression) === 'safely') {
+            if (ts.isCallExpression(node) && getText(node.expression) === 'safely') {
                 const target = node.arguments[0];
                 if (ts.isPropertyAccessExpression(target)) {
                     return ts.createBinary(target.expression, ts.SyntaxKind.AmpersandAmpersandToken, target);
@@ -10,6 +10,10 @@ export default (ctx: ts.TransformationContext, program: ts.Program): ts.Transfor
             }
             return ts.visitEachChild(node, visitor, ctx);
         }
-        return ts.visitNode(sourceFile, visitor);
+        return ts.visitEachChild(sourceFile, visitor, ctx);
+        
+        function getText(node: ts.Node) {
+            return sourceFile.text.substring(node.getStart(), node.getEnd());
+        }
     };
 };
