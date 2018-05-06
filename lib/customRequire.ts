84d50e7bd7ef2220f5852a2f9951bfd949482bf4
cody <cevek@ya.ru>
2018-05-06 16:48:28 +0500

    Fix customRequire bug

--- lib/customRequire.ts
+++ lib/customRequire.ts
@@ -6,7 +6,7 @@ export function customRequireResolve(dir: string, module: string) {
     return (Module as any)._resolveFilename(module, m);
 }
 export function customRequire(dir: string, module: string) {
-    const m = new Module('');
+    const m = new Module(dir + '/index.js');
     m.filename = dir + '/index.js';
     m.paths = (Module as any)._nodeModulePaths(dir);
     return (Module as any)._load(module, m);
