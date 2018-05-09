e6e3261d66e3f61e1bd928f0a8bc32e0cc17e1ba
Stefan Zerkalica <zerkalica@gmail.com>
2018-05-09 22:57:55 +0300

    removed customRequire, used resolve module from browserify

--- src/customRequire.ts
+++
@@ -1,7 +0,0 @@
-import Module from 'module';
-export function customRequireResolve(dir: string, module: string) {
-    const m = new Module('');
-    m.filename = dir + '/index.js';
-    m.paths = (Module as any)._nodeModulePaths(dir);
-    return (Module as any)._resolveFilename(module, m);
-}
