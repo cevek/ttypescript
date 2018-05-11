9a2f5b93d52349e0bd279d28aa98dde7eefbbb16
cody <cevek@ya.ru>
2018-05-11 13:25:37 +0500

    Fix module interop bug

--- src/tsc.ts
+++ src/tsc.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import resolve from 'resolve';
+import * as resolve from 'resolve';
 import * as vm from 'vm';
 import { patchCreateProgram } from './patchCreateProgram';
 
