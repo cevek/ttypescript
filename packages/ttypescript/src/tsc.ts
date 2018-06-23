9a001db89fbe32bffc814dcb9e13d7a6ceb7c062
cody <cevek@ya.ru>
2018-06-24 02:38:51 +0500

    fix: require typescript in tsc

--- packages/ttypescript/src/tsc.ts
+++ packages/ttypescript/src/tsc.ts
@@ -12,12 +12,13 @@ const typescriptFilename = resolve.sync('typescript/lib/typescript', opts);
 __filename = tscFileName;
 __dirname = path.dirname(__filename);
 
-let content = fs.readFileSync(typescriptFilename, 'utf8') + '\n';
 
-content += fs
-    .readFileSync(tscFileName, 'utf8')
-    .replace(/^[\s\S]+(\(function \(ts\) \{\s+function countLines[\s\S]+)$/, '$1')
-    .replace('ts.executeCommandLine(ts.sys.args);', 'module.exports = ts;');
+const content =
+    `var ts = require("${typescriptFilename}");\n` +
+    fs
+        .readFileSync(tscFileName, 'utf8')
+        .replace(/^[\s\S]+(\(function \(ts\) \{\s+function countLines[\s\S]+)$/, '$1')
+        .replace('ts.executeCommandLine(ts.sys.args);', 'module.exports = ts;');
 
 const script = new vm.Script(content, { filename: __filename });
 
