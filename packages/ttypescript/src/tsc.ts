abbdc866307825d76a6f46a1d5d56b0696f19107
cody <cevek@ya.ru>
2018-06-24 02:49:44 +0500

    fix: Refactor tsc

--- packages/ttypescript/src/tsc.ts
+++ packages/ttypescript/src/tsc.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as resolve from 'resolve';
-import * as vm from 'vm';
 import { patchCreateProgram } from './patchCreateProgram';
 
 const opts = { basedir: process.cwd() };
@@ -12,25 +11,13 @@ const typescriptFilename = resolve.sync('typescript/lib/typescript', opts);
 __filename = tscFileName;
 __dirname = path.dirname(__filename);
 
+const commandLineTsCode = fs
+    .readFileSync(tscFileName, 'utf8')
+    .replace(/^[\s\S]+(\(function \(ts\) \{\s+function countLines[\s\S]+)$/, '$1')
+    .replace('ts.executeCommandLine(ts.sys.args);', 'module.exports = ts;');
 
-const content =
-    `var ts = require("${typescriptFilename}");\n` +
-    fs
-        .readFileSync(tscFileName, 'utf8')
-        .replace(/^[\s\S]+(\(function \(ts\) \{\s+function countLines[\s\S]+)$/, '$1')
-        .replace('ts.executeCommandLine(ts.sys.args);', 'module.exports = ts;');
 
-const script = new vm.Script(content, { filename: __filename });
-
-const context = vm.createContext({
-    ...global,
-    require,
-    module: {},
-    __filename,
-    __dirname,
-});
-
-const tss = script.runInContext(context);
-patchCreateProgram(tss, true);
-
-tss.executeCommandLine(tss.sys.args);
+const ts = require(typescriptFilename);
+patchCreateProgram(ts, true);
+eval(commandLineTsCode);
+ts.executeCommandLine(ts.sys.args);
