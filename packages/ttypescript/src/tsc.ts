ad00388814a054f3b7c17ddead5fc4a2801ef4e0
Salvatore Previti <roorback@gmail.com>
2018-12-29 21:13:38 +0100

    replace eval with vm.runInThisContext

--- packages/ttypescript/src/tsc.ts
+++ packages/ttypescript/src/tsc.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as resolve from 'resolve';
 import { loadTypeScript } from './loadTypescript';
 import { dirname } from 'path';
+import { runInThisContext } from 'vm'
 
 const ts = loadTypeScript('typescript', { folder: process.cwd(), forceConfigLoad: true });
 const tscFileName = resolve.sync('typescript/lib/tsc', { basedir: process.cwd() });
@@ -10,7 +11,9 @@ const commandLineTsCode = fs
     .replace(/^[\s\S]+(\(function \(ts\) \{\s+function countLines[\s\S]+)$/, '$1')
     .replace('ts.executeCommandLine(ts.sys.args);', '');
 
-__filename = tscFileName;
-__dirname = dirname(__filename);
-eval(commandLineTsCode);
+runInThisContext(
+    `(function (exports, require, module, __filename, __dirname, ts) {${commandLineTsCode}\n});`,
+    { filename: tscFileName, lineOffset: 0, displayErrors: true }
+).call(ts, ts, require, { exports: ts }, tscFileName, dirname(tscFileName), ts);
+
 (ts as any).executeCommandLine(ts.sys.args);
