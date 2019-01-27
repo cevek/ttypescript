25312130baff8536135fda6fa151df8ed2a50d1b
cody <cevek@ya.ru>
2019-01-27 20:07:31 +0500

    Provide full fileName

--- packages/parcel-plugin/TTypescriptAsset.js
+++ packages/parcel-plugin/TTypescriptAsset.js
@@ -2,7 +2,7 @@ const vm = require('vm');
 const { createRequireFromPath } = require('module');
 const tsAssetPath = require.resolve('parcel-bundler/src/assets/TypeScriptAsset.js');
 const original = require('fs').readFileSync(tsAssetPath, 'utf8');
-const patchedAsset = original.replace(/'typescript'/g, "'ttypescript'");
+const patchedAsset = original.replace(/'typescript'/g, "'ttypescript'").replace(/this\.relativeName/g, 'this.name');
 const factory = vm.runInThisContext(
     `(function (exports, require, module, __filename, __dirname) {${patchedAsset}\n});`,
     { filename: this.filename, lineOffset: 0, displayErrors: true }
