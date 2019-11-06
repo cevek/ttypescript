c634d36a4bb83e57dfa3d249f47f154212983714
cody <cody@MacBook-Pro-Arhur.local>
2019-11-06 17:30:59 +0300

    Fix parcel plugin warning

--- packages/parcel-plugin/TTypescriptAsset.js
+++ packages/parcel-plugin/TTypescriptAsset.js
@@ -1,5 +1,5 @@
 const vm = require('vm');
-const { createRequireFromPath } = require('module');
+const { createRequire, createRequireFromPath } = require('module');
 const tsAssetPath = require.resolve('parcel-bundler/src/assets/TypeScriptAsset.js');
 const original = require('fs').readFileSync(tsAssetPath, 'utf8');
 const patchedAsset = original.replace(/'typescript'/g, "'ttypescript'").replace(/this\.relativeName/g, 'this.name');
@@ -7,5 +7,5 @@ const factory = vm.runInThisContext(
     `(function (exports, require, module, __filename, __dirname) {${patchedAsset}\n});`,
     { filename: this.filename, lineOffset: 0, displayErrors: true }
 );
-const _require = createRequireFromPath(tsAssetPath);
+const _require = createRequire ? createRequire(tsAssetPath) : createRequireFromPath(tsAssetPath);
 factory(module.exports, _require, module, __filename, __dirname);
