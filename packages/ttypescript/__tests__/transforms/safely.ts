03685c90a26243a9cb4a944b748ce3bba1ae2f49
sunrabbit123 <qudwls185@naver.com>
2023-03-22 14:34:49 +0900

    update code

--- packages/ttypescript/__tests__/transforms/safely.ts
+++ packages/ttypescript/__tests__/transforms/safely.ts
@@ -9,8 +9,8 @@ export default function myTransformerPlugin(program: ts.Program, opts?: MyPlugin
         before(ctx: ts.TransformationContext) {
             return (sourceFile: ts.SourceFile) => {
                 function visitor(node: ts.Node): ts.Node {
-                    if (ts.isCallExpression(node) && node.expression.getText() === 'safely') {
-                        const target = node.arguments[0]
+                    if (ts.isCallExpression(node) && (node.expression as any).escapedText === 'safely') {
+                        const target = node.arguments[0];
                         if (ts.isPropertyAccessExpression(target)) {
                             return ts.factory.createBinaryExpression(
                                 target.expression,
